a6c5c88d3a7a75cdbe2423cd785d496ecfe7367c
Kendall Jordan <kendallajordan@gmail.com>
2020-05-12 15:53:20 -0700

    Updates

---
+++ precache-manifest.1bcce9ced19cc5c536eaa6fce6cc4ba0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f7a47e85276c9af70673dc42d29f021",
+    "url": "/kendallajordan/React-Tutorial/index.html"
+  },
+  {
+    "revision": "d10414d707f773db3e78",
+    "url": "/kendallajordan/React-Tutorial/static/css/main.8a18c7c4.chunk.css"
+  },
+  {
+    "revision": "c750ad45df555a5a79a8",
+    "url": "/kendallajordan/React-Tutorial/static/js/2.e6844fbd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kendallajordan/React-Tutorial/static/js/2.e6844fbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d10414d707f773db3e78",
+    "url": "/kendallajordan/React-Tutorial/static/js/main.d9fc91aa.chunk.js"
+  },
+  {
+    "revision": "a87b6e7e99861360e202",
+    "url": "/kendallajordan/React-Tutorial/static/js/runtime-main.4a808208.js"
+  }
+]);
